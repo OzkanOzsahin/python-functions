c805f38311ce7479a6815eac21210cc31c09ed27
Ozkan Ozsahin <92115875+OzkanOzsahin@users.noreply.github.com>
2023-01-04 15:58:27 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# Do not modify these lines
+__winc_id__ = '49bce82ef9cc475ca3146ee15b0259d0'
+__human_name__ = 'functions'
+
+# Add your code after this line
+
+#1 greet: takes a name and returns a string in the format:
+
+def greet(name):
+    return f"Hello, {name}!"
+
+    #2 add: takes 3 numbers (integers of floats) and returns their sum.
+def add(a, b, c):
+    return a + b + c
+
+#3 positive: takes a number (integer or float) and returns whether
+# or not it is positive in the form of a boolean. You do not have to handle the case where the argument is not an int or a float.
+def positive(x):
+    return x > 0
+
+#4 negative: takes a number (integer or float) and returns whether or not it is negative in the form of a boolean. 
+#You do not have to handle the case where the argument is not an int or float.
+
+def negative(x):
+    return x < 0    
